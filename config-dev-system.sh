9faac555d1b448211d470050a7632c1ced971d8c
Ryan Cumming <etaoins@gmail.com>
2019-04-23 05:44:24 +0000

    Basic sanity fixes
    This should fix most of the failures from the first attempt at running
    this

--- config-dev-system.sh
+++ config-dev-system.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+git remote set-url origin git@github.com:etaoins/phlogiston.git
+
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
@@ -10,8 +12,9 @@ apt install -y mosh git vim llvm-7 build-essential libz-dev fish
 
 update-alternatives --set editor /usr/bin/vim.basic
 
-adduser --shell /usr/bin/fish --disabled-password --gecos "Ryan Cumming" --ingroup admin ryan
+adduser --shell /usr/bin/fish --disabled-password --gecos "Ryan Cumming" ryan
+usermod -a -G admin ryan
 echo 'ryan ALL=(ALL) NOPASSWD' >> /etc/sudoers
 
 cp -R . /home/ryan/phlogiston
-chown -R ryan.ryan /home/ryan/phlogiston
+chown -R ryan:ryan /home/ryan/phlogiston
--- config-dev-user.sh
+++ config-dev-user.sh
@@ -8,7 +8,6 @@ git clone https://github.com/etaoins/fish-config.git ~/.config/fish
 cd ~/.config/fish
 git remote set-url origin git@github.com:etaoins/fish-config.git
 cd -
-cp local.fish ~/.config/fish
 
 git clone https://github.com/etaoins/vimrc.git ~/.vim
 cd ~/.vim
@@ -22,8 +21,9 @@ git config --global user.email "etaoins@gmail.com"
 cp .gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
-curl https://sh.rustup.rs -sSf | sh -- -y
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 PATH=$PATH:~/.cargo/bin
+cp local.fish ~/.config/fish
 
 rustup component add rustfmt
 rustup component add clippy
