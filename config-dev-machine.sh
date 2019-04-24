0cad66f6ed321c8082c059d164da771000f63897
Ryan Cumming <etaoins@gmail.com>
2019-04-24 11:33:05 +0000

    Add more comments

--- config-dev-machine.sh
+++ config-dev-machine.sh
@@ -2,22 +2,32 @@
 
 set -eu
 
+# We were cloned over HTTPS; make us read/write once we have a private key
 git remote set-url origin git@github.com:etaoins/phlogiston.git
 
+# Don't prompt for package configuration
 export DEBIAN_FRONTEND=noninteractive
 
+# Update and install base packages
 apt update
 apt full-upgrade -y
 apt install -y mosh git vim llvm-7 build-essential libz-dev fish
 
+# Use Vim as root's editor
 update-alternatives --set editor /usr/bin/vim.basic
 
+# Add a `ryan` user using Fish shell and no password
 adduser --shell /usr/bin/fish --disabled-password --gecos "Ryan Cumming" ryan
 usermod -a -G admin ryan
+
+# Allow `ryan` to sudo without a password because he doesn't have one
 echo 'ryan ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/100-ryan
 
+# Make a copy of ourselves in `ryan`'s home directory
 USER_SELF_COPY=~ryan/Code/phlogiston
 mkdir -p $(dirname "${USER_SELF_COPY}")
 cp -R . "${USER_SELF_COPY}"
 chown -R ryan:ryan $(dirname "${USER_SELF_COPY}")
+
+# Execute `config-dev-user.sh` as `ryan`
 su - ryan -s /bin/sh -c "cd ${USER_SELF_COPY} && ./config-dev-user.sh"
--- config-dev-user.sh
+++ config-dev-user.sh
@@ -2,10 +2,12 @@
 
 set -eu
 
+# Allow logins from trusted private keys
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 cp authorized_keys ~/.ssh
 
+# Configure Fish shell
 mkdir -p ~/.config
 rm -Rf ~/.config/fish || true
 git clone https://github.com/etaoins/fish-config.git ~/.config/fish
@@ -13,23 +15,30 @@ cd ~/.config/fish
 git remote set-url origin git@github.com:etaoins/fish-config.git
 cd -
 
+# Configure Vim
 rm -Rf ~/.vim || true
 git clone https://github.com/etaoins/vimrc.git ~/.vim
 cd ~/.vim
 git remote set-url origin git@github.com:etaoins/vimrc.git
 cd -
 
+# Install Vim plugins
 vim +PlugUpdate +qall > /dev/null
 
+# Configure Git
 git config --global user.name "Ryan Cumming"
 git config --global user.email "etaoins@gmail.com"
 cp .gitignore_global ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
+# Install Rust and set the `PATH` for both this temporary shell and Fish
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 PATH=$PATH:~/.cargo/bin
 cp local.fish ~/.config/fish
 
+# Add some optional Rust components
 rustup component add rustfmt
 rustup component add clippy
+
+# Add ripgrep which is needed for Vim's fzf plugin
 cargo install ripgrep
