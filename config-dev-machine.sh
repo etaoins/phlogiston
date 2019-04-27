4b8a5c530ff44c521ebe8979d7dbba90060a46a2
Ryan Cumming <etaoins@gmail.com>
2019-04-28 07:20:45 +1000

    Use apt-get instead of apt
    `apt` is a user-facing tool that happens to have a lot of the same
    arguments. Scripts should use `apt-get`.

--- config-dev-machine.sh
+++ config-dev-machine.sh
@@ -6,9 +6,9 @@ set -eu
 export DEBIAN_FRONTEND=noninteractive
 
 # Update and install base packages
-apt update
-apt full-upgrade -y
-apt install -y mosh git vim llvm-7 build-essential libz-dev fish docker.io
+apt-get update
+apt-get full-upgrade -y
+apt-get install -y mosh git vim llvm-7 build-essential libz-dev fish docker.io
 
 # We were cloned over HTTPS; make us read/write once we have a private key
 git remote set-url origin git@github.com:etaoins/phlogiston.git
