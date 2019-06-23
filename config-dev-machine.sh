14dfd3fdf45c9e7cfe8747d16961b47a887ab526
Ryan Cumming <etaoins@gmail.com>
2019-06-23 14:24:44 +1000

    Add libc6-dbg
    This gives better backtraces both from `RUST_BACKTRACE` and `rust-lldb`.

--- config-dev-machine.sh
+++ config-dev-machine.sh
@@ -8,7 +8,8 @@ export DEBIAN_FRONTEND=noninteractive
 # Update and install base packages
 apt-get update
 apt-get full-upgrade -y
-apt-get install -y mosh curl sudo git vim llvm-7 lldb-7 build-essential libz-dev fish docker.io
+apt-get install -y mosh curl sudo git vim llvm-7 lldb-7 libc6-dbg \
+  build-essential libz-dev fish docker.io
 
 # We were cloned over HTTPS; make us read/write once we have a private key
 git remote set-url origin git@github.com:etaoins/phlogiston.git
