de04884feb6f29c707459ad0b4ce25e1665b999b
Ryan Cumming <etaoins@gmail.com>
2019-06-23 11:08:29 +1000

    Install lldb for rust-lldb

--- config-dev-machine.sh
+++ config-dev-machine.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Update and install base packages
 apt-get update
 apt-get full-upgrade -y
-apt-get install -y mosh curl sudo git vim llvm-7 build-essential libz-dev fish docker.io
+apt-get install -y mosh curl sudo git vim llvm-7 lldb-7 build-essential libz-dev fish docker.io
 
 # We were cloned over HTTPS; make us read/write once we have a private key
 git remote set-url origin git@github.com:etaoins/phlogiston.git
@@ -22,6 +22,9 @@ usermod -a -G docker ryan
 
 # Allow `ryan` to sudo without a password because he doesn't have one
 echo 'ryan ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/100-ryan
+
+# `rust-lldb` expects `lldb` without the version suffix
+update-alternatives --install /usr/bin/lldb lldb /usr/bin/lldb-7 100
 
 # Make a copy of ourselves in `ryan`'s home directory
 USER_SELF_COPY=~ryan/Code/phlogiston
